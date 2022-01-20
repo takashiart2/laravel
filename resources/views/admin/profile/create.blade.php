fcbde9a576d2a08c8979a50bf0f7f225a0077ebc
Takashiart2 <1603taka@gmail.com>
2022-01-20 14:23:56 +0000

    laravel2

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -10,3 +10,17 @@
     <h1>Myプロフィール作成画面</h1>
   </body>
 </html>
+
+@extends('layouts.profile')
+
+@section('title', 'プロフィールの新規作成')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 mx-auto">
+        <h2>Myプロフィール</h2>
+      </div>
+    </div>
+  </div>
+@endsection
