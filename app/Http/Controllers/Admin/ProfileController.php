f1f2eaec50664c04cbfc133539c362692a95685a
Takashiart2 <1603taka@gmail.com>
2022-01-29 13:24:26 +0000

    laravel3

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
     //
@@ -13,20 +15,21 @@ class ProfileController extends Controller
       return view('admin.profile.create');
     }
     
-    public function edit()
-    {
-      return view('admin.profile.edit');
-    }
-    
-    public function update()
-    {
-      return redirect('admin/profile/edit');
-    }
-    
     // 以下を追記
     public function create(Request $request)
     {
-      // admin/news/createにリダイレクトする
+      // Varidationを行う
+      $this->validate($request, Profile::$rules);
+      
+      $profile = new Profile;
+      $form = $request->all();
+      // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+      // データベースに保存する
+      $profile->fill($form);
+      $profile->save();
+      
+      // admin/profile/createにリダイレクトする
       return redirect('admin/profile/create');
     }
 }
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -29,25 +29,25 @@
             </ul>
           @endif
             <div class="form-group row">
-              <label class="col-md-2">氏名</label>
+              <label class="col-md-2" for="title">氏名</label>
               <div class="col-md-10">
                 <input type="text" class="form-control" name="title" value="{{ old('title') }}">
               </div>
             </div>
             <div class="form-group row">
-              <label class="col-md-2">性別</label>
+              <label class="col-md-2" for="title">性別</label>
               <div class="col-md-10">
                 <input type="text" class="form-control" name="title" value="{{ old('title') }}">
               </div>
             </div>
             <div class="form-group row">
-              <label class="col-md-2">趣味</label>
+              <label class="col-md-2" for="title">趣味</label>
               <div class="col-md-10">
                 <input type="text" class="form-control" name="title" value="{{ old('title') }}">
               </div>
             </div>
             <div class="form-group row">
-              <label class="col-md-2">自己紹介文</label>
+              <label class="col-md-2" for="body">自己紹介文</label>
               <div class="col-md-10">
                 <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
               </div>
