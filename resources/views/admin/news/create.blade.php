571c8eac98e09e1fda9c8c5b36fd887fda4608f7
Takashiart2 <1603taka@gmail.com>
2022-01-29 14:08:41 +0000

    laravel4

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -29,7 +29,7 @@
             </ul>
           @endif
             <div class="form-group row">
-              <label class="col-md-2" for="title">タイトル</label>
+              <label class="col-md-2"for="title">タイトル</label>
               <div class="col-md-10">
                 <input type="text" class="form-control" name="title" value="{{ old('title') }}">
               </div>
@@ -41,7 +41,7 @@
               </div>
             </div>
             <div class="form-group row">
-              <label class="col-md-2" for="title">画像</label>
+              <label class="col-md-2" for="image">画像</label>
               <div class="col-md-10">
                 <input type="file" class="form-control-file" name="image">
               </div>
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -29,27 +29,27 @@
             </ul>
           @endif
             <div class="form-group row">
-              <label class="col-md-2" for="title">氏名</label>
+              <label class="col-md-2" for="name">氏名</label>
               <div class="col-md-10">
-                <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                <input type="text" class="form-control" name="name" value="{{ old('name') }}">
               </div>
             </div>
             <div class="form-group row">
-              <label class="col-md-2" for="title">性別</label>
+              <label class="col-md-2" for="gender">性別</label>
               <div class="col-md-10">
-                <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                <input type="text" class="form-control" name="gender" value="{{ old('gender') }}">
               </div>
             </div>
             <div class="form-group row">
               <label class="col-md-2" for="title">趣味</label>
               <div class="col-md-10">
-                <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                <input type="text" class="form-control" name="hobby" value="{{ old('hobby') }}">
               </div>
             </div>
             <div class="form-group row">
-              <label class="col-md-2" for="body">自己紹介文</label>
+              <label class="col-md-2" for="introduction">自己紹介文</label>
               <div class="col-md-10">
-                <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
+                <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
               </div>
             </div>
             {{ csrf_field() }}
