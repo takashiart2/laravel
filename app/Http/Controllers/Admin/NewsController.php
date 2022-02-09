ae57f51ca6cec54f10f9127e9562aa477873757a
Takashiart2 <1603taka@gmail.com>
2022-02-09 10:01:26 +0000

    laravel16-2

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -5,6 +5,9 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use App\News;
+// 以下を追記
+use App\History;
+use Carbon\Carbon;
 
 class NewsController extends Controller
 {
@@ -81,12 +84,16 @@ class NewsController extends Controller
         $news_form['image_path'] = $news->image_path;
       }
       
+      unset($news_form['_token']);
       unset($news_form['image']);
       unset($news_form['remove']);
-      unset($news_form['_token']);
-      
       // 該当するデータを上書きして保存する
       $news->fill($news_form)->save();
+      
+      $history = new History();
+      $history->news_id = $news->id;
+      $history->edited_at = Carbon::now();
+      $history->save();
       
       return redirect('admin/news');
     }
--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,13 +1,13 @@
 
 @extends('layouts.profile')
 
-@section('title', 'プロフィールの新規作成')
+@section('title', 'プロフィールの編集')
 
 @section('content')
   <div class="container">
     <div class="row">
       <div class="col-md-8 mx-auto">
-        <h2>プロフィール新規作成</h2>
+        <h2>プロフィール編集</h2>
         <form action="{{ action('Admin\ProfileController@update') }}" method="post" enctype="multipart/form-data">
           @if (count($errors) > 0)
             <ul>
