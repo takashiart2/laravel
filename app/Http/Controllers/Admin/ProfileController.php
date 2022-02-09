67b156e5fcc6dac04d1f0c9a349e387406f39f5a
Takashiart2 <1603taka@gmail.com>
2022-02-09 14:20:03 +0000

    laravel16-3

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -56,7 +56,16 @@ class ProfileController extends Controller
     }
     public function update(Request $request)
     {
-      
+      // Validationをかける
+      $this->validate($request, Profile::$rules);
+      // Profile Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      // 送信されてきたフォームデータを格納する
+      $profile_form = $request->all();
+      unset($profile_form['_token']);
+      // 該当するデータを上書きして保存する
+      $profile->fill($profile_form)->save();
+      return redirect('admin/profile');
     }
   
   }
